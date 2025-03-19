f8943bbd6610e12b017714b8cdb4cb7c529eb669
aviralshukla-06 <aviralshukla733@gmail.com>
2025-03-19 11:21:57 +0530

    added some more flexible libraries like zod and jsonwebtoken

--- paytmBack/index.js
+++ paytmBack/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 
-const mainRouter = require("./routes/");
-app.use("/api/v1", mainRouter);
+// const mainRouter = require("./routes/");
+
+
+const { userRouter } = require("./routes/user")
+const { paymentRouter } = require("./routes/payment")
+
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/payment", paymentRouter);
+
+app.listen(3000);
 
--- paytmBack/routes/user.js
+++ paytmBack/routes/user.js
@@ -1,9 +1,21 @@
+const userRouter = express.Router();
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bycrypt = require("bcrypt");
+const { z } = require("zod");
+
+const secret = process.env.JWT_SECRET;
+
 
 // const mongoose= require("mongoose");
-const router = express.Router();
+
+// console.log("JT_SECRET: ", secret);
+// console.log("Loaded JWT_SECRET:", process.env.JWT_SECRET);
+
+
 
 router.get("/user", (req, res) => {
+
 
 })
 

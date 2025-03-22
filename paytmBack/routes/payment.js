b3aba41db9ee26241fbe4682920bb423efb9759a
aviralshukla-06 <aviralshukla733@gmail.com>
2025-03-23 00:32:27 +0530

    introduced payment function

--- paytmBack/routes/payment.js
+++ paytmBack/routes/payment.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const userMiddleware = require("../middlewares/usermid");
+const { default: mongoose } = require("mongoose");
 const paymentRouter = express.Router();
 
 // Define payment routes here
-paymentRouter.get("/", (req, res) => {
-    res.json({ message: "Payment API working!" });
+paymentRouter.get("/checkBal", userMiddleware, (req, res) => {
+
 });
 
 // console.log("working payment");
+
+paymentRouter.post("/transfer", userMiddleware, async function (req, res) {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+})
 
 module.exports = paymentRouter;  // ✅ Make sure it's exported like this

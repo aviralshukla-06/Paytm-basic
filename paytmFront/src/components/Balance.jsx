66c21fea7eb32dc1678ce9372d047c9d2c1c749b
aviralshukla-06 <aviralshukla733@gmail.com>
2025-04-17 13:10:57 +0530

    new commit

--- paytmFront/src/components/Balance.jsx
+++ paytmFront/src/components/Balance.jsx
@@ -1,4 +1,9 @@
+import axios from "axios";
+
 const Balance = ({ value }) => {
+
+    const value = axios.get("http://localhost:3000/api/v1/user/")
+
     return <div className="flex">
         <div className="font-bold text-lg">
             Your balance

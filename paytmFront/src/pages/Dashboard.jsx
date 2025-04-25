1b4d5af0e240b7886685d6f3b7009929d94df411
aviralshukla-06 <aviralshukla733@gmail.com>
2025-04-25 12:50:22 +0530

    modified Dashboard  page

--- paytmFront/src/pages/Dashboard.jsx
+++ paytmFront/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import Appbar from "../components/Appbar";
 import Balance from "../components/Balance";
 import Users from "../components/Users";
 
+
 const Dashboard = () => {
     return (
         <div className="min-h-screen bg-gray-100">
